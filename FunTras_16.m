c5525b6c4b9e3ec447f3200963f51b82e3d84147
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-11-24 18:21:38 -0600

    FunTras16

---
+++ FunTras_16.m
@@ -0,0 +1,26 @@
+% Octave Script
+% Title                      :FunTras_16
+% Description                :Funciones trascendentes; logarítmicas, trigonométricas y exponenciales
+% Author                     :Gustavo García Barrera(NONE_NAME)
+% Date                       :20211124
+% Version                    :1     
+% Usage                      :Este equipo> Documentos >Scripts Calculo Dif acripts calculod> Resolver_Inecuaciones
+%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :https://octaveintro.readthedocs.io/en/latest/index.html
+
+% Limpiar variables
+clear
+% Inicio de paquetería symbolic
+syms x
+% Dominio
+x=-10:0.1:10;
+% Funcion a plotear
+fx= sin((6 .*x)-10);
+% Dibujar x, fx
+plot (x, fx);
+hold on 
+grid on;
+% Título
+title(['FuncionTrascendente 16; sin clasificacion'])
+disp('Función trigonométrica');
+disp('Por las cacacterísticas que presenta la gráfica no se le puede atribuir la clasificacion a algun tipo de funcion');
