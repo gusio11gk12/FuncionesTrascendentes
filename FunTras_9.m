f28f2216e097030de4763f28d03c4b5261d273ee
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-11-24 18:16:47 -0600

    FunTras9

---
+++ FunTras_9.m
@@ -0,0 +1,26 @@
+% Octave Script
+% Title                      :FunTras_9
+% Description                :Funciones trascendentes; logarítmicas, trigonométricas y exponenciales
+% Author                     :Gustavo García Barrera(NONE_NAME)
+% Date                       :20211124
+% Version                    :1     
+% Usage                      :Este equipo> Documentos >Scripts Calculo Dif acripts calculod> Resolver_Inecuaciones
+%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :https://octaveintro.readthedocs.io/en/latest/index.html
+
+% Limpiar variables
+clear
+% Inicio de paquetería symbolic
+syms x
+% Dominio
+x=-10:0.1:10;
+% Funcion a plotear
+fx= tan(x);
+% Dibujar x, fx
+plot (x, fx);
+hold on 
+grid on;
+% Título
+title(['FuncionTrascendente 9; sobreyectiva '])
+disp('Función trigonométrica');
+disp('Funcion sobreyectiva porque a cada elemento del conjunto de llegada le corresponde por lo menos un elemento del conjunto de partida, sin importar si estos valores se repiden mas de una vez  ');
