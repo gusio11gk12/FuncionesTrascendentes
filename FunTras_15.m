d9313fa5e5cbcfef2ad78b2795a5891268c0b13e
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-11-24 18:20:54 -0600

    FunTras15

---
+++ FunTras_15.m
@@ -0,0 +1,26 @@
+% Octave Script
+% Title                      :FunTras_15
+% Description                :Funciones trascendentes; logarítmicas, trigonométricas y exponenciales
+% Author                     :Gustavo García Barrera(NONE_NAME)
+% Date                       :20211124
+% Version                    :1     
+% Usage                      :Este equipo> Documentos >Scripts Calculo Dif acripts calculod> Resolver_Inecuaciones
+%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :https://octaveintro.readthedocs.io/en/latest/index.html
+
+% Limpiar variables
+clear
+% Inicio de paquetería symbolic
+syms x
+% Dominio
+x=-10:0.1:10;
+% Funcion a plotear
+fx= acos(x);
+% Dibujar x, fx
+plot (x, fx);
+hold on 
+grid on;
+% Título
+title(['FuncionTrascendente 15; '])
+disp('Función exponencial');
+disp('Funcion inyectiva ');
